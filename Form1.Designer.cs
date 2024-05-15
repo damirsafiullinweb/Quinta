0283926d04968341feb2d9135721b00848b0b6e2
Сафиуллин Дамир <safiullin131@outlook.com>
2024-05-15 21:08:42 +0700

    Base

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            buttonStart = new Button();
+            buttonPause = new Button();
+            buttonOpen = new Button();
+            buttonStop = new Button();
+            trackBarTimer = new TrackBar();
+            trackBarVolume = new TrackBar();
+            labelTrackName = new Label();
+            labelTimer = new Label();
+            ((System.ComponentModel.ISupportInitialize)trackBarTimer).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarVolume).BeginInit();
+            SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            buttonStart.Location = new Point(51, 112);
+            buttonStart.Name = "buttonStart";
+            buttonStart.Size = new Size(94, 29);
+            buttonStart.TabIndex = 0;
+            buttonStart.Text = "старт";
+            buttonStart.UseVisualStyleBackColor = true;
+            buttonStart.Click += buttonStart_Click;
+            // 
+            // buttonPause
+            // 
+            buttonPause.Location = new Point(51, 147);
+            buttonPause.Name = "buttonPause";
+            buttonPause.Size = new Size(94, 29);
+            buttonPause.TabIndex = 1;
+            buttonPause.Text = "пауза";
+            buttonPause.UseVisualStyleBackColor = true;
+            buttonPause.Click += buttonPause_Click;
+            // 
+            // buttonOpen
+            // 
+            buttonOpen.Location = new Point(151, 112);
+            buttonOpen.Name = "buttonOpen";
+            buttonOpen.Size = new Size(94, 29);
+            buttonOpen.TabIndex = 2;
+            buttonOpen.Text = "открыть";
+            buttonOpen.UseVisualStyleBackColor = true;
+            buttonOpen.Click += buttonOpen_Click;
+            // 
+            // buttonStop
+            // 
+            buttonStop.Location = new Point(151, 147);
+            buttonStop.Name = "buttonStop";
+            buttonStop.Size = new Size(94, 29);
+            buttonStop.TabIndex = 3;
+            buttonStop.Text = "стоп";
+            buttonStop.UseVisualStyleBackColor = true;
+            buttonStop.Click += buttonStop_Click;
+            // 
+            // trackBarTimer
+            // 
+            trackBarTimer.Location = new Point(51, 35);
+            trackBarTimer.Name = "trackBarTimer";
+            trackBarTimer.Size = new Size(330, 56);
+            trackBarTimer.TabIndex = 4;
+            trackBarTimer.Scroll += trackBarTimer_Scroll;
+            // 
+            // trackBarVolume
+            // 
+            trackBarVolume.Location = new Point(251, 112);
+            trackBarVolume.Name = "trackBarVolume";
+            trackBarVolume.Size = new Size(130, 56);
+            trackBarVolume.TabIndex = 5;
+            trackBarVolume.Value = 10;
+            trackBarVolume.Scroll += trackBarVolume_Scroll;
+            // 
+            // labelTrackName
+            // 
+            labelTrackName.AutoSize = true;
+            labelTrackName.Location = new Point(58, 12);
+            labelTrackName.Name = "labelTrackName";
+            labelTrackName.Size = new Size(112, 20);
+            labelTrackName.TabIndex = 6;
+            labelTrackName.Text = "Выберите трек";
+            // 
+            // labelTimer
+            // 
+            labelTimer.AutoSize = true;
+            labelTimer.Location = new Point(58, 71);
+            labelTimer.Name = "labelTimer";
+            labelTimer.Size = new Size(44, 20);
+            labelTimer.TabIndex = 7;
+            labelTimer.Text = "00:00";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(414, 233);
+            Controls.Add(labelTimer);
+            Controls.Add(labelTrackName);
+            Controls.Add(trackBarVolume);
+            Controls.Add(trackBarTimer);
+            Controls.Add(buttonStop);
+            Controls.Add(buttonOpen);
+            Controls.Add(buttonPause);
+            Controls.Add(buttonStart);
+            Name = "Form1";
+            Text = "Quinta";
+            ((System.ComponentModel.ISupportInitialize)trackBarTimer).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBarVolume).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button buttonStart;
+        private Button buttonPause;
+        private Button buttonOpen;
+        private Button buttonStop;
+        private TrackBar trackBarTimer;
+        private TrackBar trackBarVolume;
+        private Label labelTrackName;
+        private Label labelTimer;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,107 @@
+using NAudio.Wave;
+
 namespace Quinta
 {
     public partial class Form1 : Form
     {
+        private readonly WaveOutEvent _outputDevice;
+        private AudioFileReader _audioFile;
+        private System.Windows.Forms.Timer _timer;
+
         public Form1()
         {
             InitializeComponent();
+            _outputDevice = new WaveOutEvent();
+            _timer = new System.Windows.Forms.Timer();
+            _timer.Interval = 100;
+            _timer.Tick += Timer_Tick;
+        }
+
+        //Timer
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (_audioFile != null && _outputDevice.PlaybackState == PlaybackState.Playing)
+            {
+                labelTimer.Text = _audioFile.CurrentTime.ToString(@"mm\:ss") + " / " + _audioFile.TotalTime.ToString(@"mm\:ss");
+
+                trackBarTimer.Value = (int)_audioFile.CurrentTime.TotalMilliseconds;
+            }
+        }
+
+        //Buttons
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                if (_audioFile != null)
+                {
+                    buttonStop.PerformClick();
+                }
+                string selectedFile = openFileDialog.FileName;
+                _audioFile = new AudioFileReader(selectedFile);
+                _outputDevice.Init(_audioFile);
+                trackBarTimer.Maximum = (int)_audioFile.TotalTime.TotalMilliseconds;
+
+                labelTrackName.Text = System.IO.Path.GetFileName(selectedFile);
+                buttonStart.PerformClick();
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (_audioFile != null && _outputDevice != null)
+            {
+                _outputDevice.Play();
+                _timer.Start();
+            };
+        }
+
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            if (_outputDevice != null && _audioFile != null)
+            {
+                _outputDevice.Stop();
+                _timer.Stop();
+                _audioFile.Dispose();
+                _audioFile = null;
+                trackBarTimer.Value = 0;
+
+                labelTrackName.Text = "Выберите трек";
+                labelTimer.Text = "00:00";
+            }
+        }
+
+        private void buttonPause_Click(object sender, EventArgs e)
+        {
+            if (_outputDevice != null)
+            {
+                _outputDevice.Pause();
+                _timer.Stop();
+            }
+        }
+
+        //TrackBars
+        private void trackBarTimer_Scroll(object sender, EventArgs e)
+        {
+            if (_outputDevice != null && _audioFile != null)
+            {
+                int newPosition = trackBarTimer.Value;
+                double percentage = (double)newPosition / trackBarTimer.Maximum;
+                long newPositionInBytes = (long)(_audioFile.Length * percentage);
+
+                _audioFile.Position = newPositionInBytes;
+            }
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            if (_outputDevice != null)
+            {
+                _outputDevice.Volume = (float)trackBarVolume.Value / trackBarVolume.Maximum;
+            }
         }
     }
 }
